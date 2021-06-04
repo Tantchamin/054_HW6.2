2ec93fdeb99ca566e2269291472d72c8d27e7cc4
Tantchamin <tantchamin@gmail.com>
2021-06-05 00:17:30 +0700

    054 Homework 6.2

---
+++ 054_HW6_2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _054_HW6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double BoughtPrice = double.Parse(Console.ReadLine());
+            double RealPrice = double.Parse(Console.ReadLine());
+            double Different = Math.Abs(BoughtPrice - RealPrice);
+            double Percent = (Different / RealPrice) * 100;
+
+            if (BoughtPrice > RealPrice)
+            {
+                Console.WriteLine("Price increase by {0}%", Percent);
+            }
+            else
+            {
+                Console.WriteLine("Price reduce by {0}%", Percent);
+            }
+        }
+    }
+}
